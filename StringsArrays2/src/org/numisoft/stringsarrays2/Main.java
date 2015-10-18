1ee9851e194a54b134bafa1156fd2a1fe27771e5
Alexander Kurilenok <kurilenok@gmail.com>
2015-10-19 00:39:28 +0300

    Second commit

--- StringsArrays2/src/org/numisoft/stringsarrays2/Main.java
+++ StringsArrays2/src/org/numisoft/stringsarrays2/Main.java
@@ -1,10 +1,32 @@
 package org.numisoft.stringsarrays2;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		System.out.println("Input text: ");
+
+		Scanner scanner = new Scanner(System.in);
+		String text = scanner.nextLine().toLowerCase();
+
+		String newText = text.replaceAll(" ", "");
+
+		for (char symbol : newText.toCharArray()) {
+			System.out.print(symbol + "  ");
+		}
+
+		System.out.println();
+
+		for (char symbol : newText.toCharArray()) {
+
+			if ((symbol - 'a' + 1) < 10) {
+				System.out.print(symbol - 'a' + 1 + "  ");
+			} else {
+				System.out.print(symbol - 'a' + 1 + " ");
+			}
+		}
 
 	}
-
 }
