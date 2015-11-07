6b62653ee58069aa94c6a746b203ccf37bf80d31
Alexander Kurilenok <kurilenok@gmail.com>
2015-11-08 00:22:55 +0300

    Initial commit

---
+++ Threads1/src/org/numisoft/threads1/Main.java
@@ -0,0 +1,30 @@
+package org.numisoft.threads1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		MyThread t1 = new MyThread();
+		MyThread t2 = new MyThread();
+		t1.setName("Thread 1: ");
+		t2.setName("Thread 2: ");
+
+		t1.start();
+		t2.start();
+
+	}
+
+	static synchronized void print10() {
+		int k = 0;
+		for (int i = 0; i < 25; i++) {
+
+			StringBuilder sb = new StringBuilder();
+			sb.append(Thread.currentThread().getName());
+			for (int j = 0; j < 20; j++) {
+				sb.append(++k + " ");
+
+			}
+			System.out.println(sb);
+		}
+	}
+
+}
---
+++ Threads1/src/org/numisoft/threads1/MyThread.java
@@ -0,0 +1,12 @@
+package org.numisoft.threads1;
+
+public class MyThread extends Thread {
+
+	@Override
+	public void run() {
+
+		Main.print10();
+
+	}
+
+}
