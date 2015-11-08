30c931d9ef808a23e100b6b4d5f6f4910169d55d
Alexander Kurilenok <kurilenok@gmail.com>
2015-11-08 12:50:32 +0300

    Second commit

--- Threads2/src/org/numisoft/threads2/Customer.java
+++ Threads2/src/org/numisoft/threads2/Customer.java
@@ -4,6 +4,8 @@ public class Customer implements Runnable {
 
 	private int counter;
 
+	String[] items = { "Kartoshka", "Gamburger", "McNuggets", "Kola", "Maffin", "Milkshake" };
+
 	public Customer(int counter) {
 		this.counter = counter;
 	}
@@ -14,19 +16,27 @@ public class Customer implements Runnable {
 
 		int k = 0;
 		String name = Thread.currentThread().getName();
-		for (int i = 0; i < 5; i++) {
 
-			StringBuilder sb = new StringBuilder();
-			sb.append("Customer " + counter + " @ Counter " + name.substring(name.length() - 1)
-					+ " /// ");
-			for (int j = 0; j < 20; j++) {
-				sb.append(++k);
-			}
-			System.out.println(sb);
+		for (int i = 0; i < items.length; i++) {
+
+			System.out.println("Customer " + counter + " @ Kassa "
+					+ name.substring(name.length() - 1) + ": " + items[i]);
 		}
 
-		System.out.println("Customer " + counter + " out --> Counter "
-				+ name.substring(name.length() - 1) + " is free!");
+		// for (int i = 0; i < 5; i++) {
+		//
+		// StringBuilder sb = new StringBuilder();
+		// sb.append("Customer " + counter + " @ Counter " +
+		// name.substring(name.length() - 1)
+		// + " /// ");
+		// for (int j = 0; j < 20; j++) {
+		// sb.append(++k);
+		// }
+		// System.out.println(sb);
+		// }
+
+		System.out.println("Customer " + counter + " out\nKassa "
+				+ name.substring(name.length() - 1) + ": svobodnaja kassa!");
 
 	}
 }
